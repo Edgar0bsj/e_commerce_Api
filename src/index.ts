6d7fbc1e18a6f4dcd9adabb13810ade9bea4b523
Edgar0bsj <edgarbsj.dev@gmail.com>
2025-08-26 20:55:27 -0300

    config  projeto POO

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,13 @@
-import express from "express";
-import type { Request, Response } from "express";
+import express, { Router } from "express";
+import { routes } from "./routes/user_route.js";
+
+const rota = new routes(Router);
 
 const app = express();
 app.use(express.json());
 
-type user = {
-    id: number;
-    name: string;
-    ager: number
-}
 
-const data:user[] = [];
-let id:number = 0;
-
-// Get
-app.get('/user',(req:Request, res:Response) => {
-    res.send(data)
-})
-// GetAll
-app.get('/user/:id',(req:Request, res:Response)=>{
-    let userId = Number(req.params.id);
-    let user = data.find(el => el.id === userId)
-
-    res.send(user)
-})
-// Post
-app.post('/user/submite', (req:Request, res:Response)=>{
-    let _user:user = {
-        id: ++id,
-        name: req.body.name,
-        ager: req.body.ager,
-    }
-
-    data.push(_user);
-    res.send("dados capturados com sucesso!")
-})
-// Put
-app.put('/user/edit/:id',(req: Request, res:Response)=>{
-    let id = Number(req.params.id);
-    let clientUser = req.body;
-
-    data.forEach(el => {
-        if (el.id === id) {
-            el.name = clientUser.name;
-            el.ager = clientUser.ager;
-        }
-    })
-    res.send("item alterado com sucesso!");
-})
-// Delete
-app.delete('/user/delete/:id',(req:Request, res:Response)=>{
-    let id = Number(req.params.id);
-    let userIndex = data.findIndex(el=>el.id === id);
-    data.splice(userIndex, 1);
-
-    res.send("Usuario deletado com sucesso!")
-})
+app.use('/', rota.init());
 
 app.listen(3006, function(){
     console.log("servidor rodando na porta 3006")
---
+++ src/routes/user_route.ts
@@ -0,0 +1,79 @@
+import { Router } from "express";
+import type { Request, Response } from "express";
+
+
+type user = {
+    id: number;
+    name: string;
+    ager: number
+}
+
+export class routes {
+
+    private router;
+    private users:user[] = [];
+    private id:number = 0;
+
+    constructor(expressRouter:typeof Router){
+        this.router = expressRouter();
+        this.startRouter();
+    }
+    public init(){
+        return this.router;
+    }
+
+    private startRouter(){
+        // Get
+        this.router.get('/user',(req:Request, res:Response) => {
+        res.send(this.users)
+        });
+
+        // GetAll
+        this.router.get('/user/:id',(req:Request, res:Response)=>{
+            let userId = Number(req.params.id);
+            let user = this.users.find(el => el.id === userId)
+
+            res.send(user)
+        })
+
+        // Post
+        this.router.post('/user/submite', (req:Request, res:Response)=>{
+            let _user:user = {
+                id: ++this.id,
+                name: req.body.name,
+                ager: req.body.ager,
+            }
+
+            this.users.push(_user);
+            res.send("dados capturados com sucesso!")
+        })
+
+        // Put
+        this.router.put('/user/edit/:id',(req: Request, res:Response)=>{
+            let id = Number(req.params.id);
+            let clientUser = req.body;
+
+            this.users.forEach(el => {
+                if (el.id === id) {
+                    el.name = clientUser.name;
+                    el.ager = clientUser.ager;
+                }
+            })
+            res.send("item alterado com sucesso!");
+        })
+
+
+        // Delete
+        this.router.delete('/user/delete/:id',(req:Request, res:Response)=>{
+            let id = Number(req.params.id);
+            let userIndex = this.users.findIndex(el=>el.id === id);
+            this.users.splice(userIndex, 1);
+
+            res.send("Usuario deletado com sucesso!")
+        })
+    }
+
+} 
+
+
+
